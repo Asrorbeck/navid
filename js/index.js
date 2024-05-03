24aa7fa9e847900cab19c2f82c5c8867479cc71f
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2024-05-03 10:46:15 +0500

    final

---
+++ js/index.js
@@ -0,0 +1,51 @@
+const musicControl = document.getElementById("navbar__btn");
+const audio = document.getElementById("background-music");
+const musicIcon = document.getElementById("music-icon");
+
+let isPlaying = true;
+
+var modal = document.getElementById("myModal");
+var yesBtn = document.getElementById("yesBtn");
+var noBtn = document.getElementById("noBtn");
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+yesBtn.addEventListener("click", function () {
+  audio.muted = false;
+  audio.play();
+  musicIcon.src = "./img/unmute.svg";
+  closeModal();
+
+  isPlaying = !isPlaying;
+});
+
+noBtn.addEventListener("click", function () {
+  audio.pause();
+  musicIcon.src = "./img/mute.svg";
+  closeModal();
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+window.onload = function () {
+  modal.style.display = "block";
+};
+
+musicControl.addEventListener("click", function () {
+  if (isPlaying) {
+    audio.muted = false;
+    audio.play();
+    musicIcon.src = "./img/unmute.svg";
+  } else {
+    audio.pause();
+    musicIcon.src = "./img/mute.svg";
+  }
+
+  isPlaying = !isPlaying;
+});
