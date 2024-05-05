0b5a5ad4de5dd63f7d2ffd84f91f6b55767c6eb9
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2024-05-05 10:24:35 +0500

    1

--- js/index.js
+++ js/index.js
@@ -12,27 +12,13 @@ function closeModal() {
   modal.style.display = "none";
 }
 
-function tryPlayAudio() {
-  const playPromise = audio.play();
-  if (playPromise !== undefined) {
-    playPromise
-      .then(() => {
-        // Successfully started playback
-        audio.muted = false;
-        musicIcon.src = "./img/unmute.svg";
-      })
-      .catch((error) => {
-        // Autoplay was prevented
-        console.error("Playback failed:", error);
-        musicIcon.src = "./img/mute.svg"; // Display mute icon or alert user
-      });
-  }
-}
-
 yesBtn.addEventListener("click", function () {
+  audio.muted = false;
+  audio.play();
+  musicIcon.src = "./img/unmute.svg";
   closeModal();
-  tryPlayAudio();
-  isPlaying = true;
+
+  isPlaying = !isPlaying;
 });
 
 noBtn.addEventListener("click", function () {
@@ -47,58 +33,19 @@ window.addEventListener("click", function (event) {
   }
 });
 
+window.onload = function () {
+  modal.style.display = "block";
+};
+
 musicControl.addEventListener("click", function () {
   if (isPlaying) {
+    audio.muted = false;
+    audio.play();
+    musicIcon.src = "./img/unmute.svg";
+  } else {
     audio.pause();
     musicIcon.src = "./img/mute.svg";
-  } else {
-    tryPlayAudio();
   }
+
   isPlaying = !isPlaying;
-});
-
-const slider = document.getElementById("slider");
-const container = document.getElementById("unlock-slider-container");
-const overlay = document.getElementById("overlay");
-
-let active = false;
-
-function moveSlider(e) {
-  if (!active) return;
-  const clientX = e.type.includes("mouse") ? e.clientX : e.touches[0].clientX;
-  let x =
-    clientX - container.getBoundingClientRect().left - slider.offsetWidth / 2;
-  x = Math.max(0, Math.min(x, container.offsetWidth - slider.offsetWidth));
-  slider.style.left = `${x}px`;
-}
-
-function addEventListeners(element, events, handler) {
-  events.forEach((event) =>
-    element.addEventListener(event, handler, { passive: false })
-  );
-}
-
-addEventListeners(slider, ["mousedown", "touchstart"], function (e) {
-  e.preventDefault();
-  active = true;
-  slider.style.cursor = "grabbing";
-});
-
-addEventListeners(document, ["mousemove", "touchmove"], moveSlider);
-
-addEventListeners(document, ["mouseup", "touchend"], function () {
-  if (!active) return;
-  active = false;
-  slider.style.cursor = "pointer";
-
-  if (
-    parseInt(slider.style.left, 10) >=
-    container.offsetWidth - slider.offsetWidth
-  ) {
-    overlay.style.display = "none";
-    tryPlayAudio(); // Play audio after slider is released and moved to the end
-    isPlaying = true;
-  } else {
-    slider.style.left = "0px"; // Reset slider position if not fully slid
-  }
 });
