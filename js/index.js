a74927f3fc9ee647f43c8afb7eae3a5a1a0858b9
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2024-08-06 23:59:52 +0500

    final-next

--- js/index.js
+++ js/index.js
@@ -49,3 +49,28 @@ musicControl.addEventListener("click", function () {
 
   isPlaying = !isPlaying;
 });
+
+const targetDate = new Date("2024-09-28T00:00:00").getTime();
+
+const countdown = setInterval(function () {
+  const now = new Date().getTime();
+  const timeRemaining = targetDate - now;
+
+  if (timeRemaining <= 0) {
+    clearInterval(countdown);
+  } else {
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (timeRemaining % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+  }
+}, 1000);
---
+++ js/telegram.js
@@ -0,0 +1,52 @@
+function sendTelegram() {
+  // Get form values
+  var name = document.getElementById("name").value;
+  var textMessage = document.getElementById("text").value;
+
+  // Replace 'YOUR_BOT_TOKEN' with your actual bot token
+  var botToken = "7417329571:AAE_ngkoH8kqv4m1kkcP6O2F1F2y0HIkrpk";
+
+  // Replace 'YOUR_CHAT_IDS' with an array of your actual chat IDs
+  var chatIds = ["905770018"];
+
+  // Construct the message
+  var message = `Ф.И.О: ${name}
+  
+  Ваши пожелания: ${textMessage}`;
+
+  // Telegram Bot API endpoint for sending messages
+  var apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  // Iterate through each chat ID and send the message
+  chatIds.forEach((chatId) => {
+    // Construct the data to be sent
+    var data = {
+      chat_id: chatId,
+      text: message,
+    };
+
+    // Make a POST request to the Telegram Bot API
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {})
+      .catch((error) => {
+        console.error(`Error sending message to ${chatId}:`, error);
+      });
+  });
+
+  // Show alert
+  alert("Ваша информация записана!");
+
+  // Clear the form
+  document.getElementById("name").value = "";
+  document.getElementById("text").value = "";
+
+  // Prevent the form from submitting traditionally
+  return false;
+}
